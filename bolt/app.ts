36aa0e5f39ece0bb789dc716ad66fed630547363
malewis5 <matthew.a.lewis7@gmail.com>
2025-08-28 12:21:08 -0400

    add bolt

---
+++ bolt/app.ts
@@ -0,0 +1,17 @@
+import pkg from '@slack/bolt';
+import { VercelReceiver } from '@vercel/slack-bolt';
+import registerListeners from './listeners';
+
+const { App } = pkg;
+const receiver = new VercelReceiver();
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  receiver,
+  deferInitialization: true,
+});
+
+registerListeners(app);
+
+export { app, receiver };
